f555321f3719ec77948cba760393fe4955fa2103
bob1457 <bob.yuan@yahoo.com>
2015-07-27 21:40:55 -0700

    Product listing in shop home

--- src/CommonMarket.Web/Controllers/ShopController.cs
+++ src/CommonMarket.Web/Controllers/ShopController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CommonMarket.Core.Entities;
 using CommonMarket.Core.Interface;
+using CommonMarket.Services.ProductServices;
 
 namespace CommonMarket.Web.Controllers
 {
@@ -12,11 +13,13 @@ namespace CommonMarket.Web.Controllers
     {
         private readonly ICategoryService _categoryService;
         private readonly IMerchantService _merchantServie;
+        private readonly IProductServices _productServices;
 
-        public ShopController(ICategoryService categoryService, IMerchantService merchantServie)
+        public ShopController(ICategoryService categoryService, IMerchantService merchantServie, IProductServices productServices)
         {
             _categoryService = categoryService;
             _merchantServie = merchantServie;
+            _productServices = productServices;
         }
 
         // GET: Shop
@@ -32,6 +35,14 @@ namespace CommonMarket.Web.Controllers
             IEnumerable<ProductCategory> allCategories = _categoryService.FindAllCategories();
 
             return PartialView("_CategoryList", allCategories);
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetFeaturedProducts() //Actually newest products
+        {
+            var products = _productServices.FindAllProducts().OrderByDescending(d => d.CreateDate).Take(8); //get latest or newest products (4)
+
+            return PartialView("_FeaturedList", products);
         }
     }
 }
